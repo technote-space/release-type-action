d1829569f1ebc3aa44d8559eeb2e26737acdb5a0
Technote <technote.space@gmail.com>
2020-09-26 17:15:51 +0900

    feat: use log helper

--- src/process.ts
+++ src/process.ts
@@ -1,6 +1,6 @@
 import {Context} from '@actions/github/lib/context';
 import {Octokit} from '@technote-space/github-action-helper/dist/types';
-import {Logger} from '@technote-space/github-action-helper';
+import {Logger} from '@technote-space/github-action-log-helper';
 import ApiHelper from '@technote-space/github-action-helper/dist/api-helper';
 import {setTitle, setLabels} from './utils/pulls';
 import {isTargetBranch} from './utils/misc';
--- src/utils/version.ts
+++ src/utils/version.ts
@@ -1,7 +1,8 @@
 import {Context} from '@actions/github/lib/context';
 import {Octokit} from '@technote-space/github-action-helper/dist/types';
 import {Commit, Version} from '@technote-space/github-action-version-helper';
-import {Logger, ApiHelper} from '@technote-space/github-action-helper';
+import {ApiHelper} from '@technote-space/github-action-helper';
+import {Logger} from '@technote-space/github-action-log-helper';
 import {getBreakingChangeNotes, getExcludeMessages, getMinorUpdateCommitTypes} from './misc';
 
 export const getNextVersion = async(logger: Logger, helper: ApiHelper, octokit: Octokit, context: Context): Promise<string> => Version.getNextVersion(getMinorUpdateCommitTypes(), getExcludeMessages(), getBreakingChangeNotes(), helper, octokit, context, logger);
